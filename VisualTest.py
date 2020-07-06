5eb2498ecb6954a25f20c8b0125ec72bd9bebc15
Kia <KuantumShock@gmail.com>
2020-07-06 10:00:46 -0700

    Corrected

--- VisualTest.py
+++ VisualTest.py
@@ -2,16 +2,10 @@ print("Great to see you!")
 
 print("I Added this Github")
 
-<<<<<<< HEAD
 print("and i did this in my local repository")
-=======
 print("Oopsy i did another commit in remote repository")
->>>>>>> 7b7a080f86e2dd4ec0da9b061ffe3285a0dae9f5
 
-<<<<<<< HEAD
 
 print("How Are You?")
-=======
 print("Nice to See You!")
->>>>>>> master
 
